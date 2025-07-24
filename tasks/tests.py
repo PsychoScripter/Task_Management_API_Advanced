96260f349cc5d2f90d78cb76a039f9691bc74b7e
Hesam <psychoscripter1@gmail.com>
2025-07-24 15:57:06 +0330

    fix test

--- tasks/tests.py
+++ tasks/tests.py
@@ -3,29 +3,36 @@ from django.test import TestCase
 
 # Create your tests here.
 from rest_framework.test import APITestCase
-from .models import Task
-
+from .models import Task, Project
+from authentication.models import CustomUser
 from django.urls import reverse
 from rest_framework import status
 
 class TaskAPITestCase(APITestCase):
     def setUp(self):
-        self.task = Task.objects.create(title="Test Task", description="Sample description")
+        User = CustomUser
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+
+        self.project = Project.objects.create(title="Test Project", owner=self.user)
+        self.task = Task.objects.create(title="Test Task", description="Sample description", project=self.project)
+
+        self.client.force_authenticate(user=self.user)
 
     def test_project_list(self):
+        Project.objects.create(title="Sample Project", owner=self.user)
         url = reverse('project-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue('title' in response.data[0])
+        self.assertTrue('title' in response.data['results'][0])
 
     def test_task_list(self):
         url = reverse('task-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue('title' in response.data[0])
+        self.assertTrue('title' in response.data['results'][0])
 
     def test_task_detail(self):
         url = reverse('task-detail', kwargs={'pk': self.task.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue('title' in response.data[0])
+        self.assertTrue('title' in response.data)
